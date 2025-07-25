1819c452fc52eff8582eb67055081544900baf95
0xOZ <97407385+OierZElo@users.noreply.github.com>
2025-07-25 10:09:31 +0000

    Add files via upload

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozugazag <ozugazag@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 11:51:47 by ozugazag          #+#    #+#             */
+/*   Updated: 2025/07/25 12:09:12 by ozugazag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	ft_putstr(argv[0]);
+	write(1, "\n", 1);
+	return (0);
+}
